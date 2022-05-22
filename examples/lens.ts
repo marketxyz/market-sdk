e67d06e3196b176b8d595ba85c4a85df78566d09
Apoorv <apoorvsingal0@gmail.com>
2022-05-22 15:56:58 +0530

    fix: fix ts warning at sdk.options in examples

--- examples/lens.ts
+++ examples/lens.ts
@@ -7,7 +7,7 @@ import { DEFAULT_RPC } from "./utils";
   try {
     const web3 = new Web3(DEFAULT_RPC);
     const sdk = await MarketSDK.init(web3);
-    const lens = new PoolLensV1(sdk, sdk.options.poolLens);
+    const lens = new PoolLensV1(sdk, sdk.options!.poolLens);
 
     console.log(await lens.getPublicPoolsWithData());
   } catch (error) {
